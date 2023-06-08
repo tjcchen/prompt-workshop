eede690bf70e3e4d2fa984dcb426b2096073cd0c
Andy Chen <chenyangpoppin@hotmail.com>
2023-06-08 17:45:32 +0800

    feat: add nextjs auth config

--- README.md
+++ README.md
@@ -26,8 +26,23 @@ output:
 └── utils            # utilitity functions
 ```
 
+## Others
+```bash
+# NextAuth Secret Environment Variable( generate by openssl )
+openssl rand -base64 32
+
+# remove cached .env
+git rm .env --cached
+```
+
 ## Links
 1. Google Authentication: https://console.cloud.google.com/
+
+2. MongoDB Atlas: https://www.mongodb.com/atlas
+
+3. NextAuth Documentation: https://next-auth.js.org/getting-started/example
+
+4. OpenSSL Terminal Online: https://www.cryptool.org/en/cto/openssl
 
 ## License
 This project is licensed under the terms of the MIT license.
--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -9,10 +12,38 @@ const handler = NextAuth({
     })
   ],
   async session({ session }) {
-
+    const sessionUser = await User.findOne({
+      email: session.user.email
+    });
+    session.user.id = sessionUser._id.toString();
+    return session;
   },
   async signIn({ profile }) {
+    try {
+      // serverless -> lambda -> dynamodb
+      // this kind of function opens up only when it gets called
 
+      await connectToDB();
+
+      // 1. check if a user already exists
+      const userExists = await User.findOne({
+        email: profile.email
+      });
+
+      // 2. if not, create a new user, then write to DB
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   }
 });
 
