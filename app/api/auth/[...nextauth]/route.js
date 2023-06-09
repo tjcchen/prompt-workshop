5023ef97dc5a3f479d006b7e95cf92b0f4a60f02
Andy Chen <chenyangpoppin@hotmail.com>
2023-06-09 12:19:09 +0800

    feat: adding prompt form logic

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -16,40 +16,41 @@ const handler = NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email
-    });
-    session.user.id = sessionUser._id.toString();
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      // serverless -> lambda -> dynamodb
-      // this kind of function opens up only when it gets called
-
-      await connectToDB();
-
-      // 1. check if a user already exists
-      const userExists = await User.findOne({
-        email: profile.email
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email
       });
-
-      // 2. if not, create a new user, then write to DB
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture
+      session.user.id = sessionUser?._id?.toString();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        // serverless -> lambda -> dynamodb
+        // this kind of function opens up only when it gets called
+        await connectToDB();
+  
+        // 1. check if a user already exists
+        const userExists = await User.findOne({
+          email: profile.email
         });
-      }
+  
+        // 2. if not, create a new user, then write to DB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture
+          });
+        }
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
     }
-  }
+  },
 });
 
 export { handler as GET, handler as POST };
---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,7 @@
+export const POST = async (req, res) => {
+  // TODO
+  // 1. grab the form data
+
+  // 2. write to database
+
+};
