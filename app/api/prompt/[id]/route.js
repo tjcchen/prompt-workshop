8e5ec48839bd4974ecea09f172fbdaee2c349bd2
Andy Chen <chenyangpoppin@hotmail.com>
2023-06-10 23:39:18 +0800

    feat: implement prompt PATCH(update) and delete logic

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -2,7 +2,7 @@ import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
 // GET (read)
-export const GET = async (req, { params }) => {
+export const GET = async (request, { params }) => {
   try {
     await connectToDB();
 
@@ -23,5 +23,46 @@ export const GET = async (req, { params }) => {
 };
 
 // PATCH (update)
+export const PATCH = async (request, { params }) => {
+  const { prompt, tag } = await request.json();
+
+  try {
+    await connectToDB();
+
+    const existingPrompt = await Prompt.findById(params.id);
+
+    if (!existingPrompt) {
+      return new Response("Prompot not found", { status: 404 });
+    }
+    // update existing prompt from DBs
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    await existingPrompt.save();
+
+    return new Response(JSON.stringify(existingPrompt), {
+      status: 200
+    });
+  } catch (error) {
+    return new Response("Failed to update prompt", {
+      status: 500
+    });
+  }
+};
 
 // DELETE (delete)
+export const DELETE = async (request, { params }) => {
+  try {
+    await connectToDB();
+
+    await Prompt.findByIdAndRemove(params.id);
+
+    return new Response("Prompt deleted successfully", {
+      status: 200
+    });
+  } catch (error) {
+    return new Response("Failed to delete prompt", {
+      status: 500
+    });
+  }
+};
